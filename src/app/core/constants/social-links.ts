cd5560be5e7eaa3b9e16e101618c56608f44f6d3
zrotof <manduel21@gmail.com>
2026-09-01 10:07:22 +0200

    updates

--- src/app/core/constants/social-links.ts
+++ src/app/core/constants/social-links.ts
@@ -8,13 +8,13 @@ export interface SocialLink {
 export const SOCIAL_LINKS: SocialLink[] = [
   {
     name: 'LinkedIn',
-    url: 'https://linkedin.com',
+    url: 'https://www.linkedin.com/company/evolia-tech',
     icon: 'faLinkedin',
     ariaLabel: 'LinkedIn'
   },
   {
     name: 'Facebook',
-    url: 'https://facebook.com',
+    url: 'https://www.facebook.com/profile.php?id=61579578880270',
     icon: 'faFacebook',
     ariaLabel: 'Facebook'
   },
