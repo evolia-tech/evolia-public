711eb87f040c5d4c58403c7fbd43eaff149023a4
zrotof <manduel21@gmail.com>
2026-09-15 01:21:33 +0200

    updates

--- src/app/layout/header/components/small-screen-menu/small-screen-menu.ts
+++ src/app/layout/header/components/small-screen-menu/small-screen-menu.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { QuoteService } from '../../../../core/services/quote';
+import { CalendlyService } from '../../../../core/services/calendly.service';
 
 @Component({
   selector: 'app-small-screen-menu',
@@ -12,6 +13,7 @@ import { QuoteService } from '../../../../core/services/quote';
 })
 export class SmallScreenMenu {
   public quoteService = inject(QuoteService);
+  public calendlyService = inject(CalendlyService);
   closeMenu = output<void>();
 
   faFacebook = faFacebook;
@@ -24,6 +26,11 @@ export class SmallScreenMenu {
   openQuote() {
     this.closeMenu.emit();
     this.quoteService.openQuoteForm();
+  }
+
+  openAudit() {
+    this.closeMenu.emit();
+    this.calendlyService.openAuditPopup();
   }
 }
 
